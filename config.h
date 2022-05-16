4e493fe838e3c6b6f681f2a0081d91db7042da79
Iwwww <mike.yolgin@gmail.com>
2022-05-16 22:04:35 +0300

    Change colors and opacity

--- config.h
+++ config.h
@@ -107,13 +107,13 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
-float alphaOffset = 0.0;
+float alpha = 0.75;
+float alphaOffset = 0.9;
 float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+    "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
 	"#cc241d",
 	"#98971a",
 	"#d79921",
@@ -133,7 +133,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
+	"#060706", /* 258 -> bg */
+	// "#282828", [> 258 -> bg <]
 	"#ebdbb2", /* 259 -> fg */
 };
 
